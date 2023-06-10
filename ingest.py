d3e7fee240a70ec239cf84222b60312b6ff10b0c
PromptEngineer <134474669+PromtEngineer@users.noreply.github.com>
2023-06-09 20:45:14 -0700

    This commit makes the following updates. - How using AutoModelForCausalLM for loading the model. This will speed up model inference time and reduce the [memory usage](https://github.com/PromtEngineer/localGPT/issues/99). [51](https://github.com/PromtEngineer/localGPT/issues/51) - This will take longer in loading the  model but the answers will be much better. - Adds logging to both 'ingest.py' and 'run_localGPT.py' - You can now suppress the source documents being shown in the output with the flag '--show_sources'. - Added HuggingFaceEmbeddings as an alternative if you do not have GPU or do not need HuggingFaceInstructEmbeddings model (its nearly 5GB in size). HuggingFaceInstructEmbeddings is still the default. - Replaced the model in HuggingFaceInstructEmbeddings to 'large' - Fixed the warning: [13](https://github.com/PromtEngineer/localGPT/issues/3)

--- ingest.py
+++ ingest.py
@@ -1,15 +1,20 @@
 import os
 from typing import List
+import logging
 
 import click
 from langchain.docstore.document import Document
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 
-from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, PERSIST_DIRECTORY,
+from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, INGEST_THREADS, PERSIST_DIRECTORY,
                        SOURCE_DIRECTORY)
 
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
 
 def load_single_document(file_path: str) -> Document:
     # Loads a single document from a file path
@@ -21,16 +26,46 @@ def load_single_document(file_path: str) -> Document:
         raise ValueError("Document type is undefined")
     return loader.load()[0]
 
+def load_document_batch(filepaths):
+
+    logging.info("Loading document batch")
+    # create a thread pool
+    with ThreadPoolExecutor(len(filepaths)) as exe:
+        # load files
+        futures = [exe.submit(load_single_document, name) for name in filepaths]
+        # collect data
+        data_list = [future.result() for future in futures]
+        # return data and file paths
+        return (data_list, filepaths)
 
 def load_documents(source_dir: str) -> List[Document]:
     # Loads all documents from the source documents directory
     all_files = os.listdir(source_dir)
-    docs = []
+    paths = []
     for file_path in all_files:
         file_extension = os.path.splitext(file_path)[1]
         source_file_path = os.path.join(source_dir, file_path)
         if file_extension in DOCUMENT_MAP.keys():
-            docs.append(load_single_document(source_file_path))
+            paths.append(source_file_path)
+
+    n_workers = min(INGEST_THREADS, len(paths))
+    chunksize = round(len(paths) / n_workers)
+    docs = []
+    with ProcessPoolExecutor(n_workers) as executor:
+        futures = []
+        # split the load operations into chunks
+        for i in range(0, len(paths), chunksize):
+            # select a chunk of filenames
+            filepaths = paths[i:(i + chunksize)]
+            # submit the task
+            future = executor.submit(load_document_batch, filepaths)
+            futures.append(future)
+        # process all results
+        for future in as_completed(futures):
+            # open the file and load the data
+            contents, _ = future.result()
+            docs.extend(contents)
+
     return docs
 
 
@@ -40,43 +75,32 @@ def load_documents(source_dir: str) -> List[Document]:
     default="cuda",
     type=click.Choice(
         [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ]
+            "cpu", "cuda", "ipu", "xpu", "mkldnn", "opengl", "opencl", "ideep", "hip", "ve", "fpga", "ort",
+            "xla", "lazy", "vulkan", "mps", "meta", "hpu", "mtia",
+        ],
     ),
     help="Device to run on. (Default is cuda)",
 )
 def main(device_type):
     # Load documents and split in chunks
-    print(f"Loading documents from {SOURCE_DIRECTORY}")
+    logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
     documents = load_documents(SOURCE_DIRECTORY)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
-    print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-    print(f"Split into {len(texts)} chunks of text")
+    logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+    logging.info(f"Split into {len(texts)} chunks of text")
 
     # Create embeddings
     embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-xl",
+        model_name="hkunlp/instructor-large",
         model_kwargs={"device": device_type},
     )
+    # change the embedding type here if you are running into issues.
+    # These are much smaller embeddings and will work for most appications
+    # If you use HuggingFaceEmbeddings, make sure to also use the same in the
+    # run_localGPT.py file.
+
+    # embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 
     db = Chroma.from_documents(
         texts,
@@ -89,4 +113,6 @@ def main(device_type):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s',
+                        level=logging.INFO)
     main()
--- run_localGPT.py
+++ run_localGPT.py
@@ -1,31 +1,52 @@
 import click
+import torch
+import logging
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import HuggingFacePipeline
 # from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from transformers import LlamaForCausalLM, LlamaTokenizer, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY, ROOT_DIRECTORY
+from transformers import GenerationConfig
 
-from constants import CHROMA_SETTINGS, PERSIST_DIRECTORY
 
-
-def load_model():
+def load_model(device_type):
     """
     Select a model on huggingface.
     If you are running this for the first time, it will download a model for you.
     subsequent runs will use the model from the disk.
     """
+    # The code supports all huggingface models that ends with -HF or which have a .bin file in their HF repo.
     model_id = "TheBloke/vicuna-7B-1.1-HF"
-    tokenizer = LlamaTokenizer.from_pretrained(model_id)
+    # model_id = "TheBloke/guanaco-7B-HF"
+    # model_id = 'NousResearch/Nous-Hermes-13b'
+    logging.info(f'Loading Model: {model_id}, on : {device_type}')
+    logging.info(f'This action can take a few minutes!')
 
-    model = LlamaForCausalLM.from_pretrained(
-        model_id,
-        #   load_in_8bit=True, # set these options if your GPU supports them!
-        #   device_map=1#'auto',
-        #   torch_dtype=torch.float16,
-        #   low_cpu_mem_usage=True
-    )
+    if device_type.lower() == 'cuda':
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        logging.info(f'Tokenizer loaded')
 
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map='auto',
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True
+        )
+        model.tie_weights()
+    else:
+        tokenizer = LlamaTokenizer.from_pretrained(model_id)
+        model = LlamaForCausalLM.from_pretrained(model_id, )
+
+    # load configuration from the model to avoid warnings.
+    generation_config = GenerationConfig.from_pretrained(model_id)
+    # see here for details: https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig.from_pretrained.returns
+
+    # create pipeline for text generation
     pipe = pipeline(
         "text-generation",
         model=model,
@@ -34,48 +55,61 @@ def load_model():
         temperature=0,
         top_p=0.95,
         repetition_penalty=1.15,
+        generation_config=generation_config
     )
 
     local_llm = HuggingFacePipeline(pipeline=pipe)
+    logging.info(f'Local LLM Loaded')
 
     return local_llm
 
 
+# chose device typ to run on as well as to show source documents.
 @click.command()
 @click.option(
     "--device_type",
     default="cuda",
     type=click.Choice(
         [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ]
+            "cpu", "cuda", "ipu", "xpu", "mkldnn", "opengl", "opencl", "ideep", "hip", "ve", "fpga", "ort",
+            "xla", "lazy", "vulkan", "mps", "meta", "hpu", "mtia",
+        ],
     ),
     help="Device to run on. (Default is cuda)",
 )
-def main(device_type):
-    print(f"Running on: {device_type}")
+@click.option(
+    "--show_sources",
+    default=True,
+    type=click.Choice(
+        [
+            False,
+            True,
+        ]
+    ),
+    help="Show sources along with answers (Default is Fals)",
+)
+def main(device_type, show_sources):
+    '''
+    This function implements the information retreival task.
+
+
+    1. Loads an embedding model, can be HuggingFaceInstructEmbeddings or HuggingFaceEmbeddings
+    2. Loads the existing vectorestore that was created by inget.py
+    3. Loads the local LLM using load_model function - You can now set different LLMs.
+    4. Setup the Question Answer retreival chain.
+    5. Question answers.
+    '''
+
+    logging.info(f'Running on: {device_type}')
+    logging.info(f'Display Source Documents set to: {show_sources}')
 
     embeddings = HuggingFaceInstructEmbeddings(
-        model_name="hkunlp/instructor-xl", model_kwargs={"device": device_type}
+        model_name="hkunlp/instructor-large", model_kwargs={"device": device_type}
     )
+
+    # uncomment the following line if you used HuggingFaceEmbeddings in the ingest.py
+    # embeddings = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
+
     # load the vectorstore
     db = Chroma(
         persist_directory=PERSIST_DIRECTORY,
@@ -83,10 +117,10 @@ def main(device_type):
         client_settings=CHROMA_SETTINGS,
     )
     retriever = db.as_retriever()
-    # Prepare the LLM
-    # callbacks = [StreamingStdOutCallbackHandler()]
+
     # load the LLM for generating Natural Language responses.
-    llm = load_model()
+    llm = load_model(device_type)
+
     qa = RetrievalQA.from_chain_type(
         llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True
     )
@@ -95,7 +129,6 @@ def main(device_type):
         query = input("\nEnter a query: ")
         if query == "exit":
             break
-
         # Get the answer from the chain
         res = qa(query)
         answer, docs = res["result"], res["source_documents"]
@@ -106,17 +139,20 @@ def main(device_type):
         print("\n> Answer:")
         print(answer)
 
-        # # Print the relevant sources used for the answer
-        print(
-            "----------------------------------SOURCE DOCUMENTS---------------------------"
-        )
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-        print(
-            "----------------------------------SOURCE DOCUMENTS---------------------------"
-        )
+        if show_sources:  # this is a flag that you can set to disable showing answers.
+            # # Print the relevant sources used for the answer
+            print(
+                "----------------------------------SOURCE DOCUMENTS---------------------------"
+            )
+            for document in docs:
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+            print(
+                "----------------------------------SOURCE DOCUMENTS---------------------------"
+            )
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s',
+                        level=logging.INFO)
     main()
