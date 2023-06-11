6be35c95d932d448f523e3c095269e3f52f6678e
David Tchekachev <contact@tchekda.fr>
2023-06-12 00:10:53 +0200

    docs(cuda): add max_memory example to uncomment if needed

--- run_localGPT.py
+++ run_localGPT.py
@@ -66,7 +66,8 @@ def load_model(device_type, model_id, model_basename=None):
             device_map='auto',
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True,
-            trust_remote_code=True
+            trust_remote_code=True,
+            # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
         )
         model.tie_weights()
     else:
