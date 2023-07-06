26e1dfd792e2804fb5dceb13c7c04e1f8bcba2bd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-07-06 15:11:20 -0400

    refactor: Fix and update docstrings

--- localGPT/run.py
+++ localGPT/run.py
@@ -4,38 +4,33 @@ localGPT/run.py
 This script implements the information retrieval task using
 a Question-Answer retrieval chain.
 
-The steps involved are as follows:
-1. Load an embedding model, which can be
-   HuggingFaceInstructEmbeddings or HuggingFaceEmbeddings.
-2. Load the existing vector store that was created by ingest.py.
-3. Load the local LLM using the load_model function.
-4. Set up the Question-Answer retrieval chain.
-5. Prompt the user for questions and provide answers based on the
-   retrieval chain.
-
 Usage:
     python run.py [OPTIONS]
 
 Options:
-    --repo_id TEXT      The model repository.
-                                 Default: TheBloke/WizardLM-7B-V1.0-Uncensored-GGML
-    --safetensors TEXT     The model safetensors.
-                                 Default:
-                                 WizardLM-7B-uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors
-    --embeddings_repo_id TEXT       The embedding model repository.
-                                 Default: hkunlp/instructor-large
-    --embeddings_class TEXT        The embedding model type.
-                                 Default: HuggingFaceInstructEmbeddings
-    --torch_device_type TEXT           The compute device used by the model.
-                                 Choices: cpu, cuda, ipu, xpu, mkldnn, opengl, opencl,
-                                          ideep, hip, ve, fpga, ort, xla, lazy, vulkan,
-                                          mps, meta, hpu, mtia
-                                 Default: cuda
-    --path_database TEXT     The embeddings database path.
-                                 Default: DB
+    --repo_id TEXT                 The model repository to load the LLM model from.
+                                   Default: TheBloke/WizardLM-7B-V1.0-Uncensored-GGML
+    --model_class TEXT             The model class to use.
+                                   Default: LlamaCpp
+    --safetensors TEXT             The model safetensors.
+                                   Default: WizardLM-7B-uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors
+    --embeddings_repo_id TEXT      The embedding model repository to use.
+                                   Default: hkunlp/instructor-large
+    --embeddings_class TEXT        The embedding model class to use.
+                                   Default: HuggingFaceInstructEmbeddings
+    --torch_device_type TEXT       The compute device used by the LLM model.
+                                   Choices: cpu, cuda, ipu, xpu, mkldnn, opengl, opencl,
+                                            ideep, hip, ve, fpga, ort, xla, lazy, vulkan,
+                                            mps, meta, hpu, mtia
+                                   Default: cuda
+    --path_database TEXT           The path where the embeddings database is located.
+                                   Default: DB
     --show_sources / --no_show_sources
-                                 Display the documents' source text.
-                                 Default: False
+                                   Display the source text of the retrieved documents.
+                                   Default: False
+    --use_triton / --no_use_triton
+                                   Use AMD triton for CUDA backend.
+                                   Default: False
 """
 
 import logging
@@ -67,55 +62,55 @@ from localGPT.model.loader import ModelLoader
     "--repo_id",
     default=HF_MODEL_REPO_ID,
     type=click.STRING,
-    help=f"The model repository (default: {HF_MODEL_REPO_ID})",
+    help=f"The model repository to load the LLM model from. Default is {HF_MODEL_REPO_ID}",
 )
 @click.option(
     "--model_class",
     default=LC_MODEL_CLASS,
     type=click.Choice(CHOICE_LC_MODEL_CLASSES),
-    help=f"The model type (default: {LC_MODEL_CLASS})",
+    help=f"The model class to use. Default is {LC_MODEL_CLASS}",
 )
 @click.option(
     "--safetensors",
     default=HF_MODEL_SAFETENSORS,
     type=click.Choice(CHOICE_HF_MODEL_SAFETENSORS),
-    help=f"The model safetensors (default: {HF_MODEL_SAFETENSORS})",
+    help=f"The model safetensors to use. Default is {HF_MODEL_SAFETENSORS}",
 )
 @click.option(
     "--embeddings_repo_id",
     default=HF_EMBEDDINGS_REPO_ID,
     type=click.Choice(CHOICE_HF_EMBEDDINGS_REPO_IDS),
-    help=f"The embedding model repository (default: {HF_EMBEDDINGS_REPO_ID})",
+    help=f"The embedding model repository to use. Default is {HF_EMBEDDINGS_REPO_ID}",
 )
 @click.option(
     "--embeddings_class",
     default=LC_EMBEDDINGS_CLASS,
     type=click.Choice(CHOICE_LC_EMBEDDINGS_CLASSES),
-    help=f"The embedding model type (default: {LC_EMBEDDINGS_CLASS})",
+    help=f"The embedding model class to use. Default is {LC_EMBEDDINGS_CLASS}",
 )
 @click.option(
     "--torch_device_type",
     default=TORCH_DEVICE_TYPE,
     type=click.Choice(CHOICE_PT_DEVICE_TYPES),
-    help="The compute device used by the model (default: cuda)",
+    help=f"The compute device used by the LLM model. Default is {CHOICE_PT_DEVICE_TYPES}",
 )
 @click.option(
     "--path_database",
     default=PATH_DATABASE,
     type=click.STRING,
-    help=f"The embeddings database path (default: {PATH_DATABASE})",
+    help=f"The path where the embeddings database is located. Default is {PATH_DATABASE}",
 )
 @click.option(
     "--show_sources",
     type=click.BOOL,
     default=bool(),
-    help="Display the documents source text (default: False)",
+    help="Display the source text of the retrieved documents. Default is False.",
 )
 @click.option(
     "--use_triton",
     type=click.BOOL,
     default=bool(),
-    help="Use AMD triton for CUDA backend (default: False)",
+    help="Use AMD triton for CUDA backend. Default is False.",
 )
 def main(
     model_class,
@@ -130,6 +125,17 @@ def main(
 ):
     """
     Execute the information retrieval task using a Question-Answer retrieval chain.
+
+    Args:
+        model_class (str): The model class to use.
+        repo_id (str): The model repository to load the LLM model from.
+        safetensors (str): The model safetensors to use.
+        embeddings_repo_id (str): The embedding model repository to use.
+        embeddings_class (str): The embedding model class to use.
+        torch_device_type (str): The compute device used by the LLM model.
+        path_database (str): The path where the embeddings database is located.
+        show_sources (bool): Display the source text of the retrieved documents.
+        use_triton (bool): Use AMD triton for CUDA backend.
     """
 
     # Create ChromaDBLoader instance
