ee17465b8c7375cc8e9411055e7e81c7633e8315
Jeffrey Wang <ngjaywa@gmail.com>
2023-07-23 19:34:12 -0400

    change context for llama 2

--- run_localGPT.py
+++ run_localGPT.py
@@ -64,11 +64,12 @@ def load_model(device_type, model_id, model_basename=None):
             # The code supports all huggingface models that ends with GPTQ and have some variation
             # of .no-act.order or .safetensors in their HF repo.
             logging.info("Using GGML for quantized models")
-            n_gpu_layers = 40  # Change this value based on your model and your GPU VRAM pool.
-            n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+            n_gpu_layers = 1000  # Change this value based on your model and your GPU VRAM pool.
+            n_batch = 4096  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
             callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
             model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
             return LlamaCpp(
+                n_ctx=4096,
                 model_path=model_path,
                 n_gpu_layers=n_gpu_layers,
                 n_batch=n_batch,
