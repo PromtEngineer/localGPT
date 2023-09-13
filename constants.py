a49c29a0a1abccb1bcefa77c81b3e347072742c1
PromptEngineer <134474669+PromtEngineer@users.noreply.github.com>
2023-09-13 01:04:40 -0700

    Major code update to run_localGPT
    This includes the following update.
    
    - Support for GGUF models.
    - Ability to enable/disable chat history
    - Set parameters in constants.py
    - Prompt Template for Llama-2 (this works well) and generic template for other models.
    - Major rewrite of the main run_localGPT.py as run_localGPT_v2.py (This will replace the original version after testing by the community).
    - and more :)

--- constants.py
+++ constants.py
@@ -23,6 +23,9 @@ CHROMA_SETTINGS = Settings(
     is_persistent=True,
 )
 
+# Context Window and Max New Tokens
+CONTEXT_WINDOW_SIZE = 4096
+MAX_NEW_TOKENS = CONTEXT_WINDOW_SIZE#int(CONTEXT_WINDOW_SIZE/4)
 
 # https://python.langchain.com/en/latest/_modules/langchain/document_loaders/excel.html#UnstructuredExcelLoader
 DOCUMENT_MAP = {
@@ -70,13 +73,28 @@ EMBEDDING_MODEL_NAME = "hkunlp/instructor-large" # Uses 1.5 GB of VRAM (High Acc
 ####    32b        130 GB       65 GB       32.5 GB - 35 GB    16.25 GB - 19 GB  
 ####    65b        260.8 GB     130.4 GB    65.2 GB - 67 GB    32.6 GB -  - 35 GB  
 
-MODEL_ID = "TheBloke/Llama-2-7B-Chat-GGML"
-MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+# MODEL_ID = "TheBloke/Llama-2-7B-Chat-GGML"
+# MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+
+####
+#### (FOR GGUF MODELS)
+####
+
+# MODEL_ID = "TheBloke/Llama-2-13b-Chat-GGUF"
+# MODEL_BASENAME = "llama-2-13b-chat.Q4_K_M.gguf"
+
+MODEL_ID = "TheBloke/Llama-2-7b-Chat-GGUF"
+MODEL_BASENAME = "llama-2-7b-chat.Q4_K_M.gguf"
+
+# MODEL_ID = "TheBloke/Llama-2-70b-Chat-GGUF"
+# MODEL_BASENAME = "llama-2-70b-chat.Q4_K_M.gguf"
 
 ####
 #### (FOR HF MODELS)
 ####
 
+# MODEL_ID = "NousResearch/Llama-2-7b-chat-hf"
+# MODEL_BASENAME = None
 # MODEL_ID = "TheBloke/vicuna-7B-1.1-HF"
 # MODEL_BASENAME = None
 # MODEL_ID = "TheBloke/Wizard-Vicuna-7B-Uncensored-HF"
@@ -92,43 +110,43 @@ MODEL_BASENAME = "llama-2-7b-chat.ggmlv3.q4_0.bin"
 ##### 48GB VRAM Graphics Cards (RTX 6000, RTX A6000 and other 48GB VRAM GPUs) #####
 
 ### 65b GPTQ LLM Models for 48GB GPUs (*** With best embedding model: hkunlp/instructor-xl ***)
-# model_id = "TheBloke/guanaco-65B-GPTQ"
-# model_basename = "model.safetensors"
-# model_id = "TheBloke/Airoboros-65B-GPT4-2.0-GPTQ"
-# model_basename = "model.safetensors"
-# model_id = "TheBloke/gpt4-alpaca-lora_mlp-65B-GPTQ"
-# model_basename = "model.safetensors"
-# model_id = "TheBloke/Upstage-Llama1-65B-Instruct-GPTQ" 
-# model_basename = "model.safetensors"    
+# MODEL_ID = "TheBloke/guanaco-65B-GPTQ"
+# MODEL_BASENAME = "model.safetensors"
+# MODEL_ID = "TheBloke/Airoboros-65B-GPT4-2.0-GPTQ"
+# MODEL_BASENAME = "model.safetensors"
+# MODEL_ID = "TheBloke/gpt4-alpaca-lora_mlp-65B-GPTQ"
+# MODEL_BASENAME = "model.safetensors"
+# MODEL_ID = "TheBloke/Upstage-Llama1-65B-Instruct-GPTQ" 
+# MODEL_BASENAME = "model.safetensors"    
 
 ##### 24GB VRAM Graphics Cards (RTX 3090 - RTX 4090 (35% Faster) - RTX A5000 - RTX A5500) #####
 
 ### 13b GPTQ Models for 24GB GPUs (*** With best embedding model: hkunlp/instructor-xl ***)
-# model_id = "TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ"
-# model_basename = "Wizard-Vicuna-13B-Uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors"
-# model_id = "TheBloke/vicuna-13B-v1.5-GPTQ"
-# model_basename = "model.safetensors"
-# model_id = "TheBloke/Nous-Hermes-13B-GPTQ"
-# model_basename = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
-# model_id = "TheBloke/WizardLM-13B-V1.2-GPTQ" 
-# model_basename = "gptq_model-4bit-128g.safetensors
+# MODEL_ID = "TheBloke/Wizard-Vicuna-13B-Uncensored-GPTQ"
+# MODEL_BASENAME = "Wizard-Vicuna-13B-Uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors"
+# MODEL_ID = "TheBloke/vicuna-13B-v1.5-GPTQ"
+# MODEL_BASENAME = "model.safetensors"
+# MODEL_ID = "TheBloke/Nous-Hermes-13B-GPTQ"
+# MODEL_BASENAME = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
+# MODEL_ID = "TheBloke/WizardLM-13B-V1.2-GPTQ" 
+# MODEL_BASENAME = "gptq_model-4bit-128g.safetensors
 
 ### 30b GPTQ Models for 24GB GPUs (*** Requires using intfloat/e5-base-v2 instead of hkunlp/instructor-large as embedding model ***)
-# model_id = "TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ"
-# model_basename = "Wizard-Vicuna-30B-Uncensored-GPTQ-4bit--1g.act.order.safetensors" 
-# model_id = "TheBloke/WizardLM-30B-Uncensored-GPTQ"
-# model_basename = "WizardLM-30B-Uncensored-GPTQ-4bit.act-order.safetensors" 
+# MODEL_ID = "TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ"
+# MODEL_BASENAME = "Wizard-Vicuna-30B-Uncensored-GPTQ-4bit--1g.act.order.safetensors" 
+# MODEL_ID = "TheBloke/WizardLM-30B-Uncensored-GPTQ"
+# MODEL_BASENAME = "WizardLM-30B-Uncensored-GPTQ-4bit.act-order.safetensors" 
 
 ##### 8-10GB VRAM Graphics Cards (RTX 3080 - RTX 3080 Ti - RTX 3070 Ti - 3060 Ti - RTX 2000 Series, Quadro RTX 4000, 5000, 6000) #####
 ### (*** Requires using intfloat/e5-small-v2 instead of hkunlp/instructor-large as embedding model ***)
 
 ### 7b GPTQ Models for 8GB GPUs
-# model_id = "TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ"
-# model_basename = "Wizard-Vicuna-7B-Uncensored-GPTQ-4bit-128g.no-act.order.safetensors"
-# model_id = "TheBloke/WizardLM-7B-uncensored-GPTQ"
-# model_basename = "WizardLM-7B-uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors"
-# model_id = "TheBloke/wizardLM-7B-GPTQ"
-# model_basename = "wizardLM-7B-GPTQ-4bit.compat.no-act-order.safetensors"
+# MODEL_ID = "TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ"
+# MODEL_BASENAME = "Wizard-Vicuna-7B-Uncensored-GPTQ-4bit-128g.no-act.order.safetensors"
+# MODEL_ID = "TheBloke/WizardLM-7B-uncensored-GPTQ"
+# MODEL_BASENAME = "WizardLM-7B-uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors"
+# MODEL_ID = "TheBloke/wizardLM-7B-GPTQ"
+# MODEL_BASENAME = "wizardLM-7B-GPTQ-4bit.compat.no-act-order.safetensors"
 
 ####
 #### (FOR GGML) (Quantized cpu+gpu+mps) models - check if they support llama.cpp
---
+++ load_models.py
@@ -0,0 +1,159 @@
+
+import torch
+from auto_gptq import AutoGPTQForCausalLM
+from huggingface_hub import hf_hub_download
+from langchain.llms import LlamaCpp
+
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    LlamaForCausalLM,
+    LlamaTokenizer,
+)
+from constants import (
+    CONTEXT_WINDOW_SIZE, 
+    MAX_NEW_TOKENS
+)
+
+def load_quantized_model_gguf_ggml(model_id, model_basename, device_type, logging):
+    """
+    Load a GGUF/GGML quantized model using LlamaCpp.
+
+    This function attempts to load a GGUF/GGML quantized model using the LlamaCpp library. 
+    If the model is of type GGML, and newer version of LLAMA-CPP is used which does not support GGML, 
+    it logs a message indicating that LLAMA-CPP has dropped support for GGML.
+
+    Parameters:
+    - model_id (str): The identifier for the model on HuggingFace Hub.
+    - model_basename (str): The base name of the model file.
+    - device_type (str): The type of device where the model will run, e.g., 'mps', 'cuda', etc.
+    - logging (logging.Logger): Logger instance for logging messages.
+
+    Returns:
+    - LlamaCpp: An instance of the LlamaCpp model if successful, otherwise None.
+
+    Notes:
+    - The function uses the `hf_hub_download` function to download the model from the HuggingFace Hub.
+    - The number of GPU layers is set based on the device type.
+    """
+
+    try:
+        logging.info("Using Llamacpp for GGUF/GGML quantized models")
+        model_path = hf_hub_download(
+            repo_id=model_id, 
+            filename=model_basename, 
+            resume_download=True, 
+            cache_dir="./models",
+            )
+        kwargs = {
+            "model_path": model_path,
+            "n_ctx": CONTEXT_WINDOW_SIZE,
+            "max_tokens": MAX_NEW_TOKENS,
+        }
+        if device_type.lower() == "mps":
+            kwargs["n_gpu_layers"] = 1
+        if device_type.lower() == "cuda":
+            kwargs["n_gpu_layers"] = 100 # set this based on your GPU
+
+        return LlamaCpp(**kwargs)
+    except:
+        if 'ggml' in model_basename:
+            logging.INFO("If you were using GGML model, LLAMA-CPP Dropped Support, Use GGUF Instead")
+        return None
+
+def load_quantized_model_qptq(model_id, model_basename, device_type, logging):
+
+    """
+    Load a GPTQ quantized model using AutoGPTQForCausalLM.
+
+    This function loads a quantized model that ends with GPTQ and may have variations 
+    of .no-act.order or .safetensors in their HuggingFace repo.
+
+    Parameters:
+    - model_id (str): The identifier for the model on HuggingFace Hub.
+    - model_basename (str): The base name of the model file.
+    - device_type (str): The type of device where the model will run.
+    - logging (logging.Logger): Logger instance for logging messages.
+
+    Returns:
+    - model (AutoGPTQForCausalLM): The loaded quantized model.
+    - tokenizer (AutoTokenizer): The tokenizer associated with the model.
+
+    Notes:
+    - The function checks for the ".safetensors" ending in the model_basename and removes it if present.
+    """
+        
+    # The code supports all huggingface models that ends with GPTQ and have some variation
+    # of .no-act.order or .safetensors in their HF repo.
+    logging.info("Using AutoGPTQForCausalLM for quantized models")
+
+    if ".safetensors" in model_basename:
+        # Remove the ".safetensors" ending if present
+        model_basename = model_basename.replace(".safetensors", "")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+    logging.info("Tokenizer loaded")
+
+    model = AutoGPTQForCausalLM.from_quantized(
+        model_id,
+        model_basename=model_basename,
+        use_safetensors=True,
+        trust_remote_code=True,
+        device_map="auto",
+        use_triton=False,
+        quantize_config=None,
+    )
+    return model, tokenizer
+
+def load_full_model(model_id, model_basename, device_type, logging):
+
+    """
+    Load a full model using either LlamaTokenizer or AutoModelForCausalLM.
+
+    This function loads a full model based on the specified device type. 
+    If the device type is 'mps' or 'cpu', it uses LlamaTokenizer and LlamaForCausalLM. 
+    Otherwise, it uses AutoModelForCausalLM.
+
+    Parameters:
+    - model_id (str): The identifier for the model on HuggingFace Hub.
+    - model_basename (str): The base name of the model file.
+    - device_type (str): The type of device where the model will run.
+    - logging (logging.Logger): Logger instance for logging messages.
+
+    Returns:
+    - model (Union[LlamaForCausalLM, AutoModelForCausalLM]): The loaded model.
+    - tokenizer (Union[LlamaTokenizer, AutoTokenizer]): The tokenizer associated with the model.
+
+    Notes:
+    - The function uses the `from_pretrained` method to load both the model and the tokenizer.
+    - Additional settings are provided for NVIDIA GPUs, such as loading in 4-bit and setting the compute dtype.
+    """
+
+    if device_type.lower() in ["mps", "cpu"]:
+        logging.info("Using LlamaTokenizer")
+        tokenizer = LlamaTokenizer.from_pretrained(
+            model_id, 
+            cache_dir="./models/"
+            )
+        model = LlamaForCausalLM.from_pretrained(
+            model_id, 
+            cache_dir="./models/"
+            )
+    else:
+        logging.info("Using AutoModelForCausalLM for full models")
+        tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir="./models/")
+        logging.info("Tokenizer loaded")
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id,
+            device_map="auto",
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            cache_dir="./models/",
+            # trust_remote_code=True, # set these if you are using NVIDIA GPU
+            # load_in_4bit=True,
+            # bnb_4bit_quant_type="nf4",
+            # bnb_4bit_compute_dtype=torch.float16,
+            # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
+        )
+        model.tie_weights()
+    return model, tokenizer
