b8dfbe8dcb29869b72906c55d19c23dd0d2ae868
Andrew Yu <andrewyu.au@gmail.com>
2023-11-15 14:26:54 +1100

    Update API to allow option for external access
    Similar to localGPTUI.py.

--- run_localGPT_API.py
+++ run_localGPT_API.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shutil
 import subprocess
+import argparse
 
 import torch
 from flask import Flask, jsonify, request
@@ -178,7 +179,19 @@ def prompt_route():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=5110, help="Port to run the API on. Defaults to 5110.")
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="127.0.0.1",
+        help="Host to run the UI on. Defaults to 127.0.0.1. "
+        "Set to 0.0.0.0 to make the UI externally "
+        "accessible from other devices.",
+    )
+    args = parser.parse_args()
+
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
     )
-    app.run(debug=False, port=5110)
+    app.run(debug=False, host=args.host, port=args.port)
